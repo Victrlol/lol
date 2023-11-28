68145015a0d64e3b4b1b896bb69efac9ffad19f7
Bienvenido <whatdehellbrubru@gmail.com>
2023-11-28 07:34:23 -0600

    dos numeros

--- prueba.py
+++ prueba.py
@@ -1,15 +1,17 @@
 n1=float(input("numero 1"))
 n2=float(input("numero 2"))
-n3=float(input("numero 3"))
 op=str(input("operacion"))
 if op=="suma":
-    r=n1+n2+n3
+    r=n1+n2
+    print(r)
 if op=="resta":
-    r=n1-n2-n3
+    r=n1-n2
+    print(r)
 if op=="multiplicacion":
-    r=n1*n2*n3
+    r=n1*n2
+    print(r)
 if op=="division":
-    r=n1/n2/n3
+    r=n1/n2
+    print(r)
 else:
     print("operacion no valida")
-print(op)
